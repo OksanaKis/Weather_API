b89655acce4cf9486f5d0a15c438e0729de1cdc6
OksanaKis <oksanakisetoja@gmail.com>
2025-09-20 09:30:05 -0700

    adding api

--- main.py
+++ main.py
@@ -2,15 +2,28 @@ from flask import Flask, render_template, url_for, redirect
 import os
 
 # app = Flask("Website") 
-app = Flask(__name__, static_folder="static", template_folder="templates")
+app = Flask(__name__, static_folder="static")
 
 @app.route("/") 
 def home(): 
-    return render_template("tutorial.html") 
+    return render_template("home.html") 
 
-@app.route("/about/") 
-def about(): 
-    return render_template("about.html") 
+@app.route("/api/v1/<station>/<date>") 
+def about(station, date): 
+    temperature = 23 
+    # df = pandas.read_csv("") 
+    # temperature = df.station(date)
+    # return render_template("about.html") 
+    return {
+        "station": station,
+        "date": date,
+        "temperature": temperature
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
+
 
 # @app.route("/")
 # def index():
@@ -34,4 +47,3 @@ def about():
 # print("CWD:", os.getcwd())
 # print("templates/tutorial.html:", os.path.exists("templates/tutorial.html"))
 # print("static/cat.png:", os.path.exists("static/cat.png"))
-app.run(debug=True) 
